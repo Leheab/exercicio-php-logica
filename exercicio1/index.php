851ec243058c90cdd4d9370f43afd24f93b57754
Letícia Abreu <abreuleticiahelena@gmail.com>
2025-06-17 17:29:55 -0300

    Adicionando exercicio1

---
+++ exercicio1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="PT-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/exercicio1/style.css">
+    <title>Exercicio1</title>
+</head>
+<body>
+    <h1>Estou no exercício 1</h1>
+    <form action="/exercicio1" method="post">
+         <div>
+            <label for="valuel">Digite o primeiro valor:</label>
+            <input type="number" name="valuel">
+         </div>
+         <div>
+            <label for="valuel">Digite o segundo valor:</label>
+            <input type="number" name="valuel">
+         </div>
+         <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
