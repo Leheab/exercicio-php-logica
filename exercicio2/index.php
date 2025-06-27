3b19ea371d5b95c11409a5573e8ae65c6b1e6c86
Letícia Abreu <abreuleticiahelena@gmail.com>
2025-06-27 19:59:02 -0300

    Trazer pasta  do exercicio2 da minha branch nvt-42, para nvt-48

---
+++ exercicio2/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 2</title>
+</head>
+<body>
+    <h1>Exercicio 2</h1>
+    <form action="/exercicio2" method="post">
+        <div>
+            <label for="value1">Digite o primeiro valor</label>
+            <input type="number" name="idade1">
+        </div>
+        <div>
+            <label for="value2">Digite o segundo valor</label>
+            <input type="number" name="idade2">
+        </div>
+            <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
