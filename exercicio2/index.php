27f612a538537a33a0a8ea6f33b728e4f294de27
Letícia Abreu <abreuleticiahelena@gmail.com>
2025-06-28 02:57:04 -0300

    Criando código de logica em php para o meu exercicio 2.

--- exercicio2/index.php
+++ exercicio2/index.php
@@ -10,14 +10,32 @@
     <h1>Exercicio 2</h1>
     <form action="/exercicio2" method="post">
         <div>
-            <label for="value1">Digite o primeiro valor</label>
-            <input type="number" name="idade1">
+            <label for="numero1">Digite o primeiro valor</label>
+            <input type="number" name="numero1">
         </div>
         <div>
-            <label for="value2">Digite o segundo valor</label>
-            <input type="number" name="idade2">
+            <label for="numero2">Digite o segundo valor</label>
+            <input type="number" name="numero2">
         </div>
             <input type="submit" value="Enviar">
     </form>
+
+    <?php
+        $number = $_POST["numero1"];
+        $number = $_POST["numero2"];
+
+        if ($number % 7 == 0) {
+        echo "O número $number é divisível por 7.";
+        } 
+        if ($number % 8 == 0) {
+        echo "O número $number é divisível por 8.";
+        } 
+        elseif ($number % 9 == 0) {
+        echo "O número $number é divisível por 9.";
+        } 
+        else {
+        echo "O número $number não é divisível por 7, 8 ou 9.";
+        }
+?>
 </body>
 </html>
