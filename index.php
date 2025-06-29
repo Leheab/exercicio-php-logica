bbf8674ab22b564004f38e28b767bd74486c33f7
Letícia Abreu <abreuleticiahelena@gmail.com>
2025-06-28 22:37:40 -0300

    corrigindo estilo

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/exercicio1/style.css">
+        <link rel="stylesheet" href="style.css">
     <title>Indice</title>
 </head>
 <body>
     <h1>Indice</h1>
     <a href="exercicio1/">Exercicio 1</a>
+    <a href="exercicio2/">Exercicio 2</a>
+
 </body>
 </html>
