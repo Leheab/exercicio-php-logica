f8ffb355f25ba5e0a26f1b226e7ed9492f988b5e
Letícia Abreu <abreuleticiahelena@gmail.com>
2025-07-10 18:03:11 -0300

    Reenviando todas as correções do exexercici2: constando apenas um campo de valor, indice atualizado e tambem apresentando resultado correto.

--- index.php
+++ index.php
@@ -10,6 +10,14 @@
     <h1>Indice</h1>
     <a href="exercicio1/">Exercicio 1</a>
     <a href="exercicio2/">Exercicio 2</a>
+    <a href="exercicio3/">Exercicio 3</a>
+    <a href="exercicio4/">Exercicio 4</a>
+    <a href="exercicio5/">Exercicio 5</a>
+    <a href="exercicio6/">Exercicio 6</a>
+    <a href="exercicio7/">Exercicio 7</a>
+    <a href="exercicio8/">Exercicio 8</a>
+    <a href="exercicio9/">Exercicio 9</a>
+    <a href="exercicio10/">Exercicio 10<
 
 </body>
 </html>
