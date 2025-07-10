9c89d29ce726941a529b9c3ba18384639949b4a2
Letícia Abreu <abreuleticiahelena@gmail.com>
2025-07-10 18:03:11 -0300

    Reenviando todas as correções do exexercici2: constando apenas um campo de valor, indice atualizado e tambem apresentando resultado correto.

--- exercicio2/index.php
+++ exercicio2/index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <h1>Exercicio 2</h1>
-    <form action="/exercicio2" method="post">
+    <form action="" method="post">
         <div>
             <label for="numero">Digite o primeiro valor</label>
             <input type="number" name="numero1">
@@ -17,20 +17,26 @@
     </form>
 
     <?php
-        $number = $_POST["numero1"];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	    if (isset($_POST["numero1"]) && $_POST["numero1"] !== "") {
+		$number = $_POST["numero1"];
 
-        if ($number % 7 == 0) {
-        echo "O número $number é divisível por 7.";
-        } 
-        if ($number % 8 == 0) {
-        echo "O número $number é divisível por 8.";
-        } 
-        elseif ($number % 9 == 0) {
-        echo "O número $number é divisível por 9.";
-        } 
-        else {
-        echo "O número $number não é divisível por 7, 8 ou 9.";
-        }
-    ?>
+		if ($number % 7 == 0) {
+			echo "O número $number é divisível por 7.";
+		}
+
+		if ($number % 8 == 0) {
+			echo "O número $number é divisível por 8.";
+		} elseif ($number % 9 == 0) {
+			echo "O número $number é divisível por 9.";
+		} else if ($number % 7 != 0 && $number % 8 != 0 && $number % 9 != 0) {
+		echo "O número $number não é divisível por 7, 8 ou 9.";
+	}
+	} else {
+		echo "Por favor, insira um número.";
+	}
+}
+?>
+
 </body>
 </html>
