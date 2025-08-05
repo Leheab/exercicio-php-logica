074ed11774615068360800af767cfd437340d84d
Letícia Abreu <abreuleticiahelena@gmail.com>
2025-08-05 15:13:31 -0300

    html e css seguindo os seguintes requisitos: Adicionando um html que recebe o números de 1 a 7. A estilização foi feita com adicionando alguns elementos do layout da universidade em que estudo, a partir de uma inspeção. Também inclui uma imagem de calendário de fundo, para reforçar um pouco o tema.

---
+++ exercicio6/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"/>
+    <title>Verificador de Dias da Semana:</title>
+</head>
+<body>
+    
+    <h1> Verificar Dia da Semana:</h1>
+
+    <form action="" method="post">
+        <div>
+            <label for="numero">Digite um número de 1 a 7 para verificar o dia correspondente:</label>
+            <input type="number" name="numero" required>
+        </div>
+        <div>
+            <input type="submit" value="Encontrar">
+        </div>
+    </form>
+</body>
+</html>
