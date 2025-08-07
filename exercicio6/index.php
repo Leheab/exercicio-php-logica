2a46df023396fcc6b5ee47970d4ac51e6987f380
Letícia Abreu <abreuleticiahelena@gmail.com>
2025-08-07 19:45:52 -0300

    Eviando Backend do exercicio 6: usei switch case para mostrar o dia da semana conforme o número. Se for número de 1 a 7, cai no default e avisa que é invalido.

--- exercicio6/index.php
+++ exercicio6/index.php
@@ -19,5 +19,40 @@
             <input type="submit" value="Encontrar">
         </div>
     </form>
+
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $numero = $_POST["numero"];
+
+            switch ($numero) {
+                case 1:
+                    $mensagem = "Domingo";
+                    break;
+                case 2:
+                    $mensagem = "Segunda-fEira";
+                    break;
+                case 3:
+                    $mensagem = "Terça-feira";
+                    break;
+                case 4:
+                    $mensagem = "Quarta-feira";
+                    break;
+                case 5:
+                    $mensagem = "Quinta-feira";
+                    break;
+                case 6:
+                    $mensagem = "Sexta-feira";
+                    break;
+                case 7:
+                    $mensagem = "Sabado";
+                    break;
+                default:
+                    $mensagem = "Número inválido, por favor digite  um número de 1 a 7 para corresponder a um dia da semana.";
+        }
+        
+        echo "<div class='mensagem'> Resultado: $mensagem</div>";
+    }
+?>
+
 </body>
 </html>
