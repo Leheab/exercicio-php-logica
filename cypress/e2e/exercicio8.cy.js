632549f50d08d836e98ecaf0db1b9ddc59aa05de
Letícia Abreu <abreuleticiahelena@gmail.com>
2025-09-29 23:09:08 -0300

    Enviando alterações no teste do cypress: Adicionando alteração em que o contains passe apresentar os resultados de contagem e da média, não apenas a mensagem Removendo o it Aparecer  mensagem notificando se tentar enviar vazio, que é apresentado pelo html.

--- cypress/e2e/exercicio8.cy.js
+++ cypress/e2e/exercicio8.cy.js
@@ -10,29 +10,25 @@ describe('Exercício da Média Aritmética', () => {
     cy.get('input[type="submit"]').should('be.visible')
   })
 
-  it(' Aparecer  mensagem notificando se tentar enviar vazio', () => {
-    cy.get('input[type="submit"]').click()
-  })
-
   it('Calcular e apresentar a médiaquando o número 3 é digitado', () => {
     cy.get('input[type="number"]').type('3')
     cy.get('input[type="submit"]').click()
-    cy.contains('Contagem dos números').should('be.visible')
-    cy.contains('Resultado da média').should('be.visible')
+    cy.contains('Contagem dos números: 1 2 3').should('be.visible')
+    cy.contains('Resultado da média: 2').should('be.visible')
   })
 
   it('Calcular e apresentar a média quando o número 10 é digitado', () => {
     cy.get('input[type="number"]').type('10')
     cy.get('input[type="submit"]').click()
-    cy.contains('Contagem dos números').should('be.visible')
-    cy.contains('Resultado da média').should('be.visible')
+    cy.contains('Contagem dos números: 1 2 3 4 5 6 7 8 9 10').should('be.visible')
+    cy.contains('Resultado da média: 5').should('be.visible')
   })
 
   it('Calcular e apresentar a média quando o número 50 é digitado', () => {
     cy.get('input[type="number"]').type('50')
     cy.get('input[type="submit"]').click()
-    cy.contains('Contagem dos números').should('be.visible')
-    cy.contains('Resultado da média').should('be.visible')
+    cy.contains('Contagem dos números: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50').should('be.visible')
+    cy.contains('Resultado da média: 25').should('be.visible')
   })
 
 })
