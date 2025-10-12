5feab15a81ed5ffd913224dcff5e514b2dbfc92d
Letícia Abreu <abreuleticiahelena@gmail.com>
2025-10-12 18:52:01 -0300

    Enviando atualizações de formulário do  exercício 10: foi usado autocomplete, conforme disponibilizado no materialize.

--- exercicio10/index.php
+++ exercicio10/index.php
@@ -3,8 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aluno Online</title>
     <link rel="stylesheet" href="css/style.css">
+    <title>Aluno Online</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>            
 </head>
 <body>
 
@@ -14,27 +17,45 @@
     </div>
 
     <form id="formNota">
-        <div  class="campo">
-            <label for="nomeAluno">Nome do Aluno:</label>
-            <input type="text" id="nomeAluno" required>
+      <div class="row">
+        <div class="col s12">
+          <div class="row">
+            <div class="input-field col s12">
+              <input type="text" id="nomeAluno" required>
+              <label for="nomeAluno">Nome do Aluno</label>
+            </div>
+          </div>
         </div>
+      </div>
 
-        <div class="campo">
-            <label for="nomeDisciplina">Disciplina:</label>
-            <input type="text" id="nomeDisciplina" required>
+      <div class="row">
+        <div class="col s12">
+          <div class="row">
+            <div class="input-field col s12">
+              <input type="text" id="nomeDisciplina" required>
+              <label for="nomeDisciplina">Disciplina</label>
+            </div>
+          </div>
         </div>
+      </div>
 
-        <div class="campo">
-            <label for="notaAluno">Nota:</label>
-            <input type="number" id="notaAluno" min="0" max="10" step="0.1" required>
+      <div class="row">
+        <div class="col s12">
+          <div class="row">
+            <div class="input-field col s12">
+              <input type="number" id="notaAluno" min="0" max="10" step="0.1" required>
+              <label for="notaAluno">Nota</label>
+            </div>
+          </div>
         </div>
+      </div>
 
-        <div class="campo">
-            <input type="submit" value="Registrar" class="botao">
+      <div class="row">
+        <div class="col s12 center-align">
+          <input type="submit" value="Registrar" class="botao waves-effect waves-light">
         </div>
+      </div>
     </form>
-
-    <div class="resultado"></div>
 
 </body>
 </html>
