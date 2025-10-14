485723b728e7f6e9c66e362dd90def51b488ba6c
Letícia Abreu <abreuleticiahelena@gmail.com>
2025-10-13 22:26:45 -0300

    Adicionando backend do exercicio 10: Usando as classes de tabela e cor  disponiveis no materialize Incluindo data e hora para mostrar quando o registro foi feito .

---
+++ exercicio10/src/notas.php
@@ -0,0 +1,40 @@
+<?php
+function calculaNota($nome, $disciplina, $nota): string {
+    if ($nota < 5) {
+        $situação = "Reprovado";
+        $cor = "#f44336";
+    } elseif ($nota <= 6.9) {
+        $situação = "Recuperação";
+        $cor = "#ffc107";
+    } else {
+        $situação = "Aprovado";
+        $cor = "#64dd17";
+    }
+
+    $data = date("d/m/Y H:i:s");
+
+    $resultado  = "<h2>Resultado do Aluno</h2>";
+    $resultado .= "<table class='highlight' style='margin-top: 20px; color: #212121;'>";
+    $resultado .= "  <thead>
+                        <tr>
+                          <th>Nome</th>
+                          <th>Disciplina</th>
+                          <th>Nota</th>
+                          <th>Situação</th>
+                          <th>Data e Hora</th>
+                        </tr>
+                     </thead>";
+    $resultado .= "  <tbody>";
+    $resultado .= "    <tr>";
+    $resultado .= "      <td>$nome</td>";
+    $resultado .= "      <td>$disciplina</td>";
+    $resultado .= "      <td>$nota</td>";
+    $resultado .= "      <td style='background-color: $cor; color: #212121;'>$situação</td>";
+    $resultado .= "      <td>$data</td>";
+    $resultado .= "    </tr>";
+    $resultado .= "  </tbody>";
+    $resultado .= "</table>";
+
+    return $resultado;
+}
+?>
