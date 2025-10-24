7c3cd14b4aa50631658cd4c75b7b97baee29d488
Letícia Abreu <abreuleticiahelena@gmail.com>
2025-10-23 22:59:03 -0300

    Enviando frontend do exercicio 11: A proposta é ser um exercício que escolha um número base e realize multiplicações. Possui dos campos e botão de submit. A estrutura do html e css, tem como referência o materialize.

---
+++ exercicio11/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Múltiplos</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</head>
+<body>
+
+    <div class="titulo">
+        <h3>Sequência de Múltiplos</h3>
+    </div>
+
+    <form id="formMultiplos" method="post">
+
+        <div class="row">
+            <div class="col s12">
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input type="number" id="base" name="base" min="1" max="50" required>
+                        <label for="base">Número base (de 1 a 50)</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col s12">
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input type="number" id="quantidade" name="quantidade" min="1" max="15" required>
+                        <label for="quantidade">Quantidade de Múltiplos (de 1 a 15)</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col s12 center-align">
+                <button type="submit" class="btn">Gerar e Salvar</button>
+            </div>
+        </div>
+
+    </form>
+
+    <div class="resultado center-align">
+        <?= $mensagem ?>
+    </div>
+
+</body>
+</html>
