80d4723c799da78ac8f9ca0c38429930520c239b
Letícia Abreu <abreuleticiahelena@gmail.com>
2025-10-30 23:17:10 -0300

    Enviando banco de dados exercício 11: Feito include, criado a planilha no php admin e vinculo o MySql.

--- exercicio11/src/gerar.php
+++ exercicio11/src/gerar.php
@@ -1,21 +1,32 @@
 <?php
+include "conexao.php";
+
 function calculaMultiplos($base, $quantidade) {
+    global $conexao;
+
     $base = (int)$base;
     $quantidade = (int)$quantidade;
 
-    $valores = [];
     $soma = 0;
     $mensagem = "Múltiplos de $base: <br>";
 
     for ($posição = 1; $posição <= $quantidade; $posição++) {
         $valor = $base * $posição;
-        $valores[] = $valor;
         $soma += $valor;
-
-        if ($posição > 1) $mensagem .= " ";
         $mensagem .= "$base X $posição = $valor<br>";
     }
 
+    $media = $soma / $quantidade;
+
+    $sql = "INSERT INTO exercicio11 (base_number, quantity, average, created_at)
+        VALUES ('$base', '$quantidade', '$media', NOW())";
+
+        if (mysqli_query($conexao, $sql)) {
+       $mensagem .= "<br>Dados inseridos com sucesso!<br>";
+    } else {
+        $mensagem .= "<br>Erro ao inserir dados: " . mysqli_error($conexao) . "<br>";
+    }
+    
     $media = $soma / $quantidade;
 
     $mensagem .= ".<br>";
@@ -24,3 +35,14 @@ function calculaMultiplos($base, $quantidade) {
 
     return $mensagem;
 }
+
+function getHistorico() {
+    global $conexao;
+    $res = mysqli_query($conexao, "SELECT base_number, quantity, average, created_at FROM exercicio11 ORDER BY created_at DESC");
+    $html = '';
+    while ($row = mysqli_fetch_assoc($res)) {
+        $html .= "Base: {$row['base_number']} | Quantidade: {$row['quantity']} | Média: {$row['average']} | Data: {$row['created_at']}<br>";
+    }
+    return $html;
+}
+?>
