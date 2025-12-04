b4e62485594b77025d28b2f1c1ceaf3982737039
Letícia Abreu <abreuleticiahelena@gmail.com>
2025-12-04 17:09:14 -0300

    Enviando alterações de segurança do exercicio 1 e 2 conforme sugerido em revisão:
        • Adicionei validação usando filter_input para garantir que somente números válidos sejam enviados.
        • Usei htmlspecialchars para evitar problemas ao exibir valores enviados pelo usuário.
        • Incluí um captcha simples para reforçar a segurança dos formulários.

--- exercicio1/index.php
+++ exercicio1/index.php
@@ -1,5 +1,14 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['captcha'])) {
+    $_SESSION['captcha'] = rand(1000, 9999);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="PT-Br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,40 +16,55 @@
     <link rel="stylesheet" href="/exercicio1/style.css">
     <title>Exercicio1</title>
 </head>
+
 <body>
     <h1>Estou no exercício 1</h1>
     <form action="" method="post">
-         <div>
+        <div>
             <label for="valuel">Digite o primeiro valor:</label>
-            <input type="number" name="value1">
-         </div>
-         <div>
+            <input type="number" name="value1" required>
+        </div>
+        <div>
             <label for="valuel">Digite o segundo valor:</label>
-            <input type="number" name="value2">
-         </div>
-         <input type="submit" value="Enviar">
+            <input type="number" name="value2" required>
+        </div>
+
+        <div>
+            <label>Digite o Captcha: <strong><?php echo $_SESSION['captcha']; ?></strong></label>
+            <input type="number" name="captcha_input" required>
+        </div>
+
+        <input type="submit" value="Enviar">
     </form>
 
     <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-            $value1 = filter_input(INPUT_POST, 'value1', FILTER_VALIDATE_INT);
-            $value2 = filter_input(INPUT_POST, 'value2', FILTER_VALIDATE_INT);
+        if ($_POST['captcha_input'] != $_SESSION['captcha']) {
+            echo "<div class='result erro'>Captcha incorreto. Tente novamente.</div>";
 
-            if ($value1 === false || $value2 === false) {
-                echo "<div class='result erro'>Por favor, digite números válidos.</div>";
-            } else {
+            $_SESSION['captcha'] = rand(1000, 9999);
+            exit;
+        }
+        $value1 = filter_input(INPUT_POST, 'value1', FILTER_VALIDATE_INT);
+        $value2 = filter_input(INPUT_POST, 'value2', FILTER_VALIDATE_INT);
+
+        if ($value1 === false || $value2 === false) {
+            echo "<div class='result erro'>Por favor, digite números válidos.</div>";
+        } else {
             $total =  $value1 +  $value2;
 
             if ($total > 20) {
                 $total = $total + 8;
-            }
-            else {
+            } else {
                 $total = $total - 5;
             }
             echo "<div class='result'>O resultado foi:" . htmlspecialchars($total) . "</div>";
         }
-    }    
+
+        $_SESSION['captcha'] = rand(1000, 9999);
+    }
     ?>
 </body>
+
 </html>
