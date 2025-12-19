dab848cdfff6240594a9787aa3d7cabc65290d98
Letícia Abreu <abreuleticiahelena@gmail.com>
2025-12-18 23:43:32 -0300

    Enviando teste cypress exercicio 8
    - Corrigi os valores das médias nos testes para 5,50 e 25,50. - Ajustei o texto esperado para o formato de vírgula. - Melhorei a leitura do resultado para ignorar interferências de tags HTML.

--- cypress/e2e/exercicio8.cy.js
+++ cypress/e2e/exercicio8.cy.js
@@ -13,22 +13,26 @@ describe('Exercício da Média Aritmética', () => {
   it('Calcular e apresentar a médiaquando o número 3 é digitado', () => {
     cy.get('input[type="number"]').type('3')
     cy.get('input[type="submit"]').click()
-    cy.contains('Contagem dos números: 1 2 3').should('be.visible')
-    cy.contains('Resultado da média: 2').should('be.visible')
+    cy.get('#resultado').should('contain', 'Contagem dos números:')
+    cy.get('#resultado').should('contain', '1 2 3')
+    cy.get('#resultado').should('contain', 'Resultado da média: 2,00')
   })
 
   it('Calcular e apresentar a média quando o número 10 é digitado', () => {
     cy.get('input[type="number"]').type('10')
     cy.get('input[type="submit"]').click()
-    cy.contains('Contagem dos números: 1 2 3 4 5 6 7 8 9 10').should('be.visible')
-    cy.contains('Resultado da média: 5').should('be.visible')
+
+    cy.get('#resultado').contains(/Contagem dos números:.*1 2 3 4 5 6 7 8 9 10/)
+    cy.get('#resultado').should('contain', 'Resultado da média: 5,50')
   })
 
   it('Calcular e apresentar a média quando o número 50 é digitado', () => {
     cy.get('input[type="number"]').type('50')
     cy.get('input[type="submit"]').click()
-    cy.contains('Contagem dos números: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50').should('be.visible')
-    cy.contains('Resultado da média: 25').should('be.visible')
+    cy.get('#resultado').should('contain', 'Contagem dos números:')
+    cy.get('#resultado').should('contain', '1 2 3 4 5')
+    cy.get('#resultado').should('contain', '46 47 48 49 50')
+    cy.get('#resultado').should('contain', 'Resultado da média: 25,50')
   })
 
 })
