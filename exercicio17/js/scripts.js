e535b4f5f3955f0aa5affb22d2a08abf1e52c455
Letícia Abreu <abreuleticiahelena@gmail.com>
2026-01-15 19:25:14 -0300

    Enviando alterações do JS no frontend conforme orientação do Edmar.

--- exercicio17/js/scripts.js
+++ exercicio17/js/scripts.js
@@ -1,30 +1,37 @@
 document.getElementById('formulario-temperatura').addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const formulario = document.getElementById('formulario-temperatura');
-    const dadosFormulario = new FormData(formulario);
+    const inputs = document.querySelectorAll('.input-temperatura');
+    const temperaturas = Array.from(inputs).map(input => parseFloat(input.value));
+
+    const formData = new FormData();
+    temperaturas.forEach(temp => formData.append('temperaturas[]', temp));
 
     fetch('processar.php', {
         method: 'POST',
-        body: dadosFormulario
+        body: formData
     })
-        .then(response => response.json())
+        .then(response => response.json()) // O PHP vai devolver os cálculos prontos
         .then(dados => {
-            document.getElementById('corpo-tabela-resultado').innerHTML = `
-            <tr>
-                <td class="valor-destaque">${dados.media}°C</td>
-                <td class="valor-destaque red-text text-darken-2">${dados.maxima}°C</td>
-                <td class="valor-destaque blue-text text-darken-2">${dados.minima}°C</td>
-                <td class="valor-destaque">${dados.percentual}%</td>
-            </tr>
-        `;
-
-            const painel = document.getElementById('painel-resultados');
-            painel.style.display = 'block';
-            painel.scrollIntoView({ behavior: 'smooth' });
+            atualizarInterface(dados);
         })
         .catch(error => {
-            console.error('Erro:', error);
-            alert('Erro ao processar os dados no servidor.');
+            console.error('Erro ao processar:', error);
+            alert('Erro ao enviar dados para o servidor.');
         });
+});
+
+function atualizarInterface(dados) {
+    document.getElementById('corpo-tabela-resultado').innerHTML = `
+            <td class="valor-destaque">${dados.media}°C</td>
+            <td class="valor-destaque red-text text-darken-2">${dados.maxima}°C</td>
+            <td class="valor-destaque blue-text text-darken-2">${dados.minima}°C</td>
+            <td class="valor-destaque">${dados.percentual}%</td>
+        </tr>
+    `;
+
+    const painel = document.getElementById('painel-resultados');
+    painel.style.display = 'block';
+
+    painel.scrollIntoView({ behavior: 'smooth' });
 });
